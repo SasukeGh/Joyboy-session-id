de203da3ae122da41dd22cc7e542e82d40ff3bc9
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2024-08-18 12:34:24 +0000

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "Mirage md v 1.5.2-wer123ghbkjnhbf";
+  let characters = "Sasuke Md v 1.0.0-wer123ghbkjnhbf";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
